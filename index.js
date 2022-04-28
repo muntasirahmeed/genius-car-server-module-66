27683f997c0faad2db600976c8bb1b83c12cf422
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-04-28 16:06:30 +0600

    module done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,6 +12,20 @@ app.use(cors());
 app.use(express.json());
 //main area
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsdjy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -21,7 +37,16 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db("geniusUser").collection("services");
     const orderCollection = client.db("geniusUser").collection("order");
+    /* --------------Auth here----------------- */
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
+    /* -------------Servie Api Here------------------ */
     //get Data:
     app.get("/services", async (req, res) => {
       const query = {};
@@ -52,15 +77,19 @@ async function run() {
       const result = await serviceCollection.deleteOne(query);
       res.send(result);
     });
-    /*  ------------------------------------------------------
-                    order collection api here 
-     ------------------------------------------------------ */
+    /* -------------Order Api Here------------------ */
     //get
-    app.get("/order", async (req, res) => {
-      const query = {};
-      const cursor = orderCollection.find(query);
-      const order = await cursor.toArray();
-      res.send(order);
+    app.get("/order", verifyJWT, async (req, res) => {
+      const decoded = req.decoded.email;
+      const email = req.query.email;
+      if (email === decoded) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
     });
 
     //post
